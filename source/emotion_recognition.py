0cb8c18ba906ce2cf3d08dc32d3c4c3228e40a55
sammy <samuel_tiong@hotmail.com>
2018-10-03 20:04:13 +0800

    Update emotion_recognition.py

--- source/emotion_recognition.py
+++ source/emotion_recognition.py
@@ -6,7 +6,7 @@
 Emotion recognition using SVMs (Scikit-learn & OpenCV
 ======================================================
 
-Dependencies: Python 3, Scikit-Learn, OpenCV 3.0.0,
+Dependencies: Python, Scikit-Learn, OpenCV,
               Numpy, Scipy, Matplotlib, Tkinter
 
 The dataset used in this example is Olivetti Faces:
@@ -39,139 +39,11 @@ print(__doc__)
 with sqlite3.connect('database.db') as db:
     c=db.cursor()
 
-c.execute('CREATE TABLE IF NOT EXISTS graph(happy NUMBER NOT NULL, sad NUMBER NOT NULL);')
+c.execute('CREATE TABLE IF NOT EXISTS graph(times TEXT NOT NULL, happy NUMBER NOT NULL, sad NUMBER NOT NULL);')
+
+c.execute('CREATE TABLE IF NOT EXISTS pie(happynum NUMBER NOT NULL, sadnum NUMBER NOT NULL,total NUMBER NOT NULL);')
 db.commit()
 db.close()
-
-faces = datasets.fetch_olivetti_faces()
-
-# ==========================================================================
-# Traverses through the dataset by incrementing index & records the result
-# ==========================================================================
-class Trainer:
-    def __init__(self):
-        self.results = {}
-        self.imgs = faces.images
-        self.index = 0
-
-    def reset(self):
-        print ("============================================")
-        print ("Resetting Dataset & Previous Results.. Done!")
-        print ("============================================")
-        self.results = {}
-        self.imgs = faces.images
-        self.index = 0
-
-    def increment_face(self):
-        if self.index + 1 >= len(self.imgs):
-            return self.index
-        else:
-            while str(self.index) in self.results:
-                # print self.index
-                self.index += 1
-            return self.index
-
-    def record_result(self, smile=True):
-        print ("Image", self.index + 1, ":", "Happy" if smile is True else "Sad")
-        self.results[str(self.index)] = smile
-
-
-
-# ===================================
-# Callback function for the buttons
-# ===================================
-## smileCallback()              : Gets called when "Happy" Button is pressed
-## noSmileCallback()            : Gets called when "Sad" Button is pressed
-## updateImageCount()           : Displays the number of images processed
-## displayFace()                : Gets called internally by either of the button presses
-## displayBarGraph(isBarGraph)  : computes the bar graph after classification is completed 100%
-## _begin()                     : Resets the Dataset & Starts from the beginning
-## _quit()                      : Quits the Application
-## printAndSaveResult()         : Save and print the classification result
-## loadResult()                 : Loading the previously stored classification result
-## run_once(m)                  : Decorator to allow functions to run only once
-
-def run_once(m):
-    def wrapper(*args, **kwargs):
-        if not wrapper.has_run:
-            wrapper.has_run = True
-            return m(*args, **kwargs)
-    wrapper.has_run = False
-    return wrapper
-
-def smileCallback():
-    trainer.record_result(smile=True)
-    trainer.increment_face()
-    displayFace(trainer.imgs[trainer.index])
-    updateImageCount(happyCount=True, sadCount= False)
-
-
-def noSmileCallback():
-    trainer.record_result(smile=False)
-    trainer.increment_face()
-    displayFace(trainer.imgs[trainer.index])
-    updateImageCount(happyCount=False, sadCount=True)
-
-
-def updateImageCount(happyCount, sadCount):
-    global HCount, SCount, imageCountString, countString   # Updating only when called by smileCallback/noSmileCallback
-    if happyCount is True and HCount < 400:
-        HCount += 1
-    if sadCount is True and SCount < 400:
-        SCount += 1
-    if HCount == 400 or SCount == 400:
-        HCount = 0
-        SCount = 0
-    # --- Updating Labels
-    # -- Main Count
-    imageCountPercentage = str(float((trainer.index + 1) * 0.25)) \
-        if trainer.index+1 < len(faces.images) else "Classification DONE! 100"
-    imageCountString = "Image Index: " + str(trainer.index+1) + "/400   " + "[" + imageCountPercentage + " %]"
-    labelVar.set(imageCountString)           # Updating the Label (ImageCount)
-    # -- Individual Counts
-    countString = "(Happy: " + str(HCount) + "   " + "Sad: " + str(SCount) + ")\n"
-    countVar.set(countString)
-
-
-@run_once
-def displayBarGraph(isBarGraph):
-    ax[1].axis(isBarGraph)
-    n_groups = 1                    # Data to plot
-    Happy, Sad = (sum([trainer.results[x] == True for x in trainer.results]),
-               sum([trainer.results[x] == False for x in trainer.results]))
-    index = np.arange(n_groups)     # Create Plot
-    bar_width = 0.5
-    opacity = 0.75
-    ax[1].bar(index, Happy, bar_width, alpha=opacity, color='b', label='Happy')
-    ax[1].bar(index + bar_width, Sad, bar_width, alpha=opacity, color='g', label='Sad')
-    ax[1].set_ylim(0, max(Happy, Sad)+10)
-    ax[1].set_xlabel('Expression')
-    ax[1].set_ylabel('Number of Images')
-    ax[1].set_title('Training Data Classification')
-    ax[1].legend()
-
-
-@run_once
-def printAndSaveResult():
-    print (trainer.results)                      # Prints the results
-    with open("../results/results.xml", 'w') as output:
-        json.dump(trainer.results, output)        # Saving The Result
-
-@run_once
-def loadResult():
-    results = json.load(open("../results/results.xml"))
-    trainer.results = results
-
-
-def displayFace(face):
-    ax[0].imshow(face, cmap='gray')
-    isBarGraph = 'on' if trainer.index+1 == len(faces.images) else 'off'      # Switching Bar Graph ON
-    if isBarGraph is 'on':
-        displayBarGraph(isBarGraph)
-        printAndSaveResult()
-    # f.tight_layout()
-    canvas.draw()
-
 
 def _opencv():
     print ("\n\n Please Wait. . . .")
@@ -181,6 +53,9 @@ def _opencv():
     #opencvProcess = subprocess.Popen("train_classifier_videofeed.py", close_fds=True, shell=True)
     # os.system('"Train Classifier.exe"')
     # opencvProcess.communicate()
+
+def _trainingdata():
+    Popen('python trainingdata.py', shell='true')
 
 def _linegraph():
     c.execute('SELECT rowid, happy, sad FROM graph')
@@ -193,29 +68,31 @@ def _linegraph():
         emotion1.append(row[1])
         emotion2.append(row[2])
 
-    plt.plot(ids,emotion1,'-')
-    plt.plot(ids,emotion2,color='orange')   
+    plt.plot(ids,emotion1,color='green')
+    #plt.plot(ids,emotion2,color='orange')   
     plt.xlabel('Time')
     plt.ylabel('Percentage (%)') 
     plt.show()
 
 def _piechart():
-    c.execute('SELECT happy,sad FROM graph')
+    c.execute('SELECT happynum,sadnum,total FROM pie')
     happy =[]
     sad = []
-    total=[]
+    datatotal=[]
+    totals=[]
     color = ['green','blue']
-    label = ['happy', 'sad']
+    label = ['happy', 'not happy']
 
     for row in c.fetchall():
         happy.append(row[0])
         sad.append(row[1])
+        datatotal.append(row[2])
 
-    total.append(sum(happy))
+    totals.append((sum(happy))/(sum(datatotal)))
 
-    total.append(sum(sad))
+    totals.append((sum(sad))/(sum(datatotal)))
 
-    plt.pie(total,labels=label,colors=color)
+    plt.pie(totals,labels=label,colors=color,autopct='%1.1f%%')
     plt.axis('equal')
     plt.title('Overall Customer Emotion', bbox={'facecolor':'0.8','pad':5})
     fig = plt.gcf()         
@@ -248,16 +125,15 @@ if __name__ == "__main__":
     root = Tk.Tk()
     root.wm_title("Emotion Recognition Using Scikit-Learn & OpenCV")
 
-    # =======================================
-    # Class Instances & Starting the Plot
-    # =======================================
-    trainer = Trainer()
 
     labels = Tk.Label(master=root, text='Emotion Analytics System')
     labels.config(font=("Courier", 18))
     labels.pack(padx=10,pady=50,side=Tk.TOP)
 
-    opencvButton = Tk.Button(master=root, text='Load the "Trained Classifier" & Test Output', command=_opencv)
+    traindataButton = Tk.Button(master=root, text='Train data', command=_trainingdata)
+    traindataButton.pack(side=Tk.TOP)
+
+    opencvButton = Tk.Button(master=root, text='Livefeed Cam', command=_opencv)
     opencvButton.pack(side=Tk.TOP)
 
     lineButton = Tk.Button(master=root, text='View Line Graph', command=_linegraph)
